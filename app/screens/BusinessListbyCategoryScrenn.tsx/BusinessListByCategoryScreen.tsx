1d785512fe1d405949fb1b4ea680b1065cffa9d1
nabilqistubillah <bilqisbill02@gmail.com>
2025-05-05 22:17:19 +0700

    menambahkan list kategory

--- app/screens/BusinessListbyCategoryScrenn.tsx/BusinessListByCategoryScreen.tsx
+++ app/screens/BusinessListbyCategoryScrenn.tsx/BusinessListByCategoryScreen.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useRoute } from '@react-navigation/native';
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useRoute, useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import GlobalAPI from '@/app/Utils/GlobalAPI';
+
 
 export default function BusinessListByCategoryScreen() {
-    const param=useRoute();
+    const param = useRoute().params as { category: string };
+    const navigation = useNavigation();
+    const [businessList, setBusinessList] = useState([]);
+
+    useEffect(()=>{
+      param && getBusinessByCategory();
+    },[param])
+
+    const getBusinessByCategory=()=>{
+      GlobalAPI.getBussinesListByCategory(param.category).then(resp=>{
+        setBusinessList(resp.businessLists);
+      })
+    }
   return (
-    <View>
-      <Text>BusinessListByCategoryScreen</Text>
+    <View style={{padding:20,paddingTop:30}}>
+      <TouchableOpacity style={{display:'flex',flexDirection:'row',gap:10,alignItems:'center'}}
+      onPress={()=> navigation.goBack()}
+      >
+        <Ionicons name="arrow-back-outline" size={30} color="black"/>
+        <Text style={{fontSize:25,fontFamily:'Outfit-Medium'}}>{param?.category}</Text>
+      </TouchableOpacity>
     </View>
   )
 }
